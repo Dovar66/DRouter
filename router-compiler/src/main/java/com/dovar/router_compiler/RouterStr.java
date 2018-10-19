549980161599fef607b94534516c2155cd79169a
heweizong@itouchtv.cn <heweizong@itouchtv.cn>
2018-10-19 10:28:14 +0800

    增加开发分支

---
+++ router-compiler/src/main/java/com/dovar/router_compiler/RouterStr.java
@@ -0,0 +1,15 @@
+package com.dovar.router_compiler;
+
+/**
+ * @Date: 2018/10/19
+ * @Author: heweizong
+ * @Description: compiler使用到的静态常量
+ */
+public interface RouterStr {
+    String proxyClassPackage = "com.dovar.router";
+    String proxyClassSimpleName = "RouterInitProxy";
+    String routerInjectorPackage = "com.dovar.router_api.compiler";
+    String routerInjectorSimpleName = "RouterInjector";
+    String BaseApplicationLogicPackage = "com.dovar.router_api.router";
+    String BaseApplicationLogicSimpleName = "BaseApplicationLogic";
+}
